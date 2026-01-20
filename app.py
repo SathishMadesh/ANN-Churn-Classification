5a032ea7652d157a45f28b60cbed70f6f48d3392
SathishMadesh <s747272946@gmail.com>
2026-01-20 23:54:46 +0530

    commiting unstaged changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import StanderdScaler, LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pickle
 
 # Load the trained model
@@ -24,7 +24,7 @@ st.title("Customer Churn Prediction")
 
 # Input fields
 geography = st.selectbox("Geography", onehot_encoder_geo.categories_[0])
-gender = st.selectbox("Gender", label_encoder_gender.classes)
+gender = st.selectbox("Gender", label_encoder_gender.classes_)
 age = st.slider("Age", 18, 92)
 balance = st.number_input("Balance", min_value=0.0)
 num_of_products = st.slider("Number of Products", 1, 4)
@@ -39,11 +39,12 @@ input_data = pd.DataFrame({
     'CreditScore': [credit_score],
     'Gender': [label_encoder_gender.transform([gender])[0]],
     'Age': [age],
+    'Tenure': [tenure],
     'Balance': [balance],
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'Tenure': [tenure]
+    'EstimatedSalary': [estimated_salary]
 })
 
 #OneHot encode Geography
@@ -52,7 +53,7 @@ geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_featur
 
 # Concatenate One Hot Encoded Geography with input data
 
-input_data = pd.concat([input_data, reset_index(drop=True), geo_encoded_df], axis=1)
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
 # Scale the input data
 input_scaled = scaler.transform(input_data)
@@ -60,6 +61,8 @@ input_scaled = scaler.transform(input_data)
 # Prediction
 prediction = model.predict(input_scaled)
 prediction_prob = prediction[0][0]
+
+st.write(f"Predicted Churn Probability: {prediction_prob:.2f}")
 
 # Display result
 if st.button("Predict Churn"):
